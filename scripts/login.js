127056e0d964c2e6710556c38ce7f258490ec1f5
André Filho <AndreFilhoSan@gmail.com>
2022-03-21 22:14:40 -0300

    Login Update

--- scripts/login.js
+++ scripts/login.js
@@ -1,3 +1,4 @@
+const loginLogout = document.querySelector('.openLogin');
 const loginBtn = document.querySelector('.signInBtn');
 const signUpBtn = document.querySelector('.signUpBtn');
 const emailInput = document.querySelector('#InputEmail');
@@ -7,36 +8,46 @@ let logged = localStorage.getItem('logged');
 // Check Login Status
 const checkLogin = () => {
     if (logged !== true) {
-      window.alert('Você precisa estar autenticado para utilizar o sistema.')
-      window.location.href="index.html"
+        window.alert('Você precisa estar autenticado para utilizar o sistema.')
+        window.location.href = "index.html"
     }
 }
 
 // Check Credentials
-const login = () =>{
+const login = () => {
     localStorage.setItem('email', emailInput.value);
     localStorage.setItem('password', passwordInput.value);
 
     let email = window.localStorage.getItem('email')
     let password = window.localStorage.getItem('password')
 
-    if(emailInput.value === email && passwordInput.value === password)
-    {
+    if (logged === true) {
+        window.alert('Usuário já logado no sistema.');
+    } else if (emailInput.value === '' || passwordInput.value === '') {
+        window.alert('Por favor preencha todos os campos.')
+    } else if (emailInput.value === email && passwordInput.value === password) {
         localStorage.setItem('logged', true);
-    } else {
+        loginLogout.innerText = 'Logout';
+        loginLogout.classList.remove('btn-primary')
+        loginLogout.classList.add('btn-warning')
+    }
+    else {
         alert('Usuário ou senha inválido.')
     }
 }
 
-// Check if all fields are filled
-const validateLogin = () => {
-    if (emailInput.value === '' || passwordInput.value === '') {
-        window.alert('Por favor preencha todos os campos.')
-    } else if (logged === true ) {
-        window.alert('Você já está logado')
-    } else{
-        login()
+const logout = () => {
+    if (loginLogout.innerText === 'Logout') {
+        window.localStorage.clear();
+        loginLogout.innerText = 'Login';
+        loginLogout.classList.remove('btn-warning')
+        loginLogout.classList.add('btn-primary')
+    } else {
+    login();
     }
 }
 
-loginBtn.addEventListener('click',validateLogin)
+loginBtn.addEventListener('click', login)
+loginLogout.addEventListener('click', logout)
+
+
